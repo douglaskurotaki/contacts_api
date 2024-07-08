cd4c488ec999ba64834bb5ef31b592f30f48397b
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-08 04:06:15 -0300

    feat: Add devise_token_auth route for User model

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
     end
