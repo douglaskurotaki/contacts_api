09514d9c8f4ac68fc42713058f7feadeed26c436
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-08 16:13:03 -0300

    refactor: Update Devise modules in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :contacts, dependent: :destroy
 end
