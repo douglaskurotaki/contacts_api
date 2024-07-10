5e22859e0b5fb365a36c0da5086c0ba1f74b899f
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-09 21:13:09 -0300

    feat: Add Gmail configuration for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "contacts_api_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'http://18.215.161.30:3000/api', protocol: 'http' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
