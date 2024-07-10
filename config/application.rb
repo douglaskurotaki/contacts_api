5eb310182834cc60162ed8fd2926c943d6d4825d
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:09:41 -0300

    style: Update application configuration with frozen string literal

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,7 +16,11 @@ module ContactsApi
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.available_locales = %i[en pt-BR]
+    config.i18n.fallbacks = %i[pt en]
 
     # Configuration for the application, engines, and railties goes here.
     #
