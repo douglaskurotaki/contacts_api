885d73e0a309930f7f97d8631bb58e0adcdabca4
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:11:50 -0300

    style: Add frozen string literal and refactor associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class Contact < ApplicationRecord
   belongs_to :user
+  has_one :address
+  accepts_nested_attributes_for :address
 
   validates :name, :cpf, :phone, presence: true
 end
