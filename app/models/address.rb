920ef90fab185b302e692ec31a887106ead94a35
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:11:30 -0300

    refactor: Update association and geolocation logic in Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,19 @@
-class Address < ApplicationRecord
-  has_one :contact
+# frozen_string_literal: true
 
-  validates :country, :street, :number, :zipcode, :uf, :latitude, :longitude, presence: true
+class Address < ApplicationRecord
+  belongs_to :contact
+
+  validates :city, :street, :number, :zipcode, :uf, :latitude, :longitude, presence: true
+
+  before_save :set_geolocation
+
+  def set_geolocation
+    return unless zipcode_changed?
+
+    geolocation = FindGeolocationUseCase.call(zipcode:)
+    return unless geolocation
+
+    self.latitude = geolocation[:latitude]
+    self.longitude = geolocation[:longitude]
+  end
 end
