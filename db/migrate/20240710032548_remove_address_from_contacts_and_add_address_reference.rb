b982dca05ae1a8a08a3edf21f2a62ced655b76cd
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:08:52 -0300

    style: Add frozen string literal comment and update syntax

--- db/migrate/20240710032548_remove_address_from_contacts_and_add_address_reference.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAddressFromContactsAndAddAddressReference < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :contacts, :address, :string
-    remove_column :contacts, :latitude, :float
-    remove_column :contacts, :longitude, :float
-
-    add_reference :contacts, :address, null: false, foreign_key: true
-  end
-end
