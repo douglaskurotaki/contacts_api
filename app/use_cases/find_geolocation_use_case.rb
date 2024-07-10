3b7f4eefbfe6e5f04e3321ba8cb810242bdfd4e6
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:11:21 -0300

    feat: Add FindGeolocationUseCase to fetch geolocation

---
+++ app/use_cases/find_geolocation_use_case.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class FindGeolocationUseCase
+  def self.call(zipcode:, gateway: GoogleMapsGateway)
+    new(zipcode:, gateway:).call
+  end
+
+  def initialize(zipcode:, gateway: GoogleMapsGateway)
+    @zipcode = zipcode
+    @gateway = gateway
+  end
+
+  def call
+    return nil unless address || geolocation
+
+    { latitude:, longitude: }
+  end
+
+  private
+
+  attr_reader :zipcode, :gateway
+
+  def address
+    @address ||= gateway.by_zipcode(zipcode:)
+  rescue StandardError => e
+    Rails.logger.error("[#{self.class}] Error fetching address from Google Maps API: #{e.message}")
+    raise e
+  end
+
+  def geolocation
+    address.dig('results', 0, 'geometry', 'location')
+  end
+
+  def latitude
+    geolocation['lat']
+  end
+
+  def longitude
+    geolocation['lng']
+  end
+end
