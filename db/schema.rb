4779ea6a22cb655a5fa15c1c71b7b22892ab3f6a
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 04:09:52 -0300

    style: Update quotes to single quotes in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,66 +12,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_032548) do
+ActiveRecord::Schema[7.1].define(version: 20_240_710_032_220) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "addresses", force: :cascade do |t|
-    t.string "country", null: false
-    t.string "street", null: false
-    t.string "uf", null: false
-    t.string "neighborhood", null: false
-    t.string "zipcode", null: false
-    t.float "latitude", null: false
-    t.float "longitude", null: false
-    t.string "number", null: false
-    t.string "complement"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'addresses', force: :cascade do |t|
+    t.bigint 'contact_id', null: false
+    t.string 'city', null: false
+    t.string 'street', null: false
+    t.string 'uf', null: false
+    t.string 'neighborhood', null: false
+    t.string 'zipcode', null: false
+    t.float 'latitude', null: false
+    t.float 'longitude', null: false
+    t.string 'number', null: false
+    t.string 'complement'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['contact_id'], name: 'index_addresses_on_contact_id'
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.string "cpf"
-    t.string "phone", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "address_id", null: false
-    t.index ["address_id"], name: "index_contacts_on_address_id"
-    t.index ["user_id"], name: "index_contacts_on_user_id"
+  create_table 'contacts', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'name', null: false
+    t.string 'cpf'
+    t.string 'phone', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_contacts_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
-    t.string "email", default: "", null: false
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'provider', default: 'email', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.boolean 'allow_password_change', default: false
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.inet 'current_sign_in_ip'
+    t.inet 'last_sign_in_ip'
+    t.string 'name'
+    t.string 'nickname'
+    t.string 'image'
+    t.string 'email', default: '', null: false
+    t.json 'tokens'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index %w[uid provider], name: 'index_users_on_uid_and_provider', unique: true
   end
 
-  add_foreign_key "contacts", "addresses"
-  add_foreign_key "contacts", "users"
+  add_foreign_key 'addresses', 'contacts'
+  add_foreign_key 'contacts', 'users'
 end
