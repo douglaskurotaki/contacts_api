ef7e9e7cdbe5fe4ff88370d431578a065189ed38
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-07-10 21:28:49 -0300

    refactor: Add validation methods for CPF and phone numbers

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,4 +6,26 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :address
 
   validates :name, :cpf, :phone, presence: true
+
+  before_validation :validate_cpf, :validate_phone
+
+  def validate_cpf
+    return if cpf.blank?
+
+    normalized_cpf = cpf.gsub(/[^0-9]/, '')
+    self.cpf = normalized_cpf
+    return if CPF.valid?(normalized_cpf)
+
+    errors.add(:cpf, I18n.t('errors.messages.invalid_format', attribute: 'CPF'))
+  end
+
+  def validate_phone
+    return if phone.blank?
+
+    normalized_phone = phone.gsub(/[^0-9]/, '')
+    self.phone = normalized_phone
+    return if normalized_phone.match?(/\A\d{10,11}\z/)
+
+    errors.add(:phone, I18n.t('errors.messages.invalid_format', attribute: 'Telefone'))
+  end
 end
